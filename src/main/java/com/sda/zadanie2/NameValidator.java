67fbfc95a578694c3bd79cf7f5252eba911913cb
szjozi <szjozefczuk.sda@gmail.com>
2022-07-09 10:13:19 +0200

    JUnit5

--- src/main/java/com/sda/zadanie2/NameValidator.java
+++ src/main/java/com/sda/zadanie2/NameValidator.java
@@ -3,6 +3,9 @@ package com.sda.zadanie2;
 public class NameValidator {
 
     public boolean isNameValid(final String name) {
-        return name != null && name.length() > 0 && !name.toUpperCase().equals(name) && !name.toLowerCase().equals(name);
+        return name != null
+                && name.length() > 0
+                && !name.toUpperCase().equals(name)
+                && !name.toLowerCase().equals(name);
     }
 }
