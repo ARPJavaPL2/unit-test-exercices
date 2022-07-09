ba060c909c169d09861811368ff2ebe35d4708c3
szjozi <szjozefczuk.sda@gmail.com>
2022-07-09 10:14:28 +0200

    JUnit5

---
+++ src/test/java/com/sda/zadanie2/NameValidatorTest.java
@@ -0,0 +1,22 @@
+package com.sda.zadanie2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameValidatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void shouldReturnFalseIfNameNull() {
+    }
+}
